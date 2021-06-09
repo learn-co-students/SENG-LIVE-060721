225d34894c55f03bd115d0839393789630c2cc60
ixnp <enprocopios@gmail.com>
2021-06-09 07:54:52 -0700

    added inventory

--- phase-1/03_higher_order_functions/books.js
+++ phase-1/03_higher_order_functions/books.js
@@ -5,31 +5,36 @@ const bookStore = {
             title: 'Eloquent JavaScript: A Modern Introduction to Programming',
             author: 'Marjin Haverbeke',
             price: 10,
-            reviews: [{user_id: 1, content:'Good book, but not great for new coders'}]
+            reviews: [{user_id: 1, content:'Good book, but not great for new coders'}],
+            inventory: 10
         },
         {
             title: 'JavaScript & JQuery: Interactive Front-End Web Development',
             author: 'Jon Duckett',
             price: 45,
-            reviews: [{user_id: 15, content:'good way to learn JQuery'}]
+            reviews: [{user_id: 15, content:'good way to learn JQuery'}],
+            inventory: 2
         },
         {
             title: 'JavaScript: The Good Parts',
             author: 'Douglas Crockford',
             price: 36,
-            reviews: [{user_id: 25, content:'I disagree with everything in this book'}, {user_id: 250, content:'Only JS book anyone needs'}]
+            reviews: [{user_id: 25, content:'I disagree with everything in this book'}, {user_id: 250, content:'Only JS book anyone needs'}],
+            inventory: 8
         },
         {
             title: 'JavaScript: The Definitive Guide',
             author: 'David Flanagan',
             price: 25,
-            reviews: [{user_id: 44, content:'Great intro to js book'}, {user_id: 350, content:'It really is the Definitive guide'}]
+            reviews: [{user_id: 44, content:'Great intro to js book'}, {user_id: 350, content:'It really is the Definitive guide'}],
+            inventory: 0
         },
         {
             title: 'You Don’t Know JS',
             author: 'David Flanagan',
             price: 6,
-            reviews: [{user_id: 76, content:'You can find this for free online, no need to pay for it!'}]
+            reviews: [{user_id: 76, content:'You can find this for free online, no need to pay for it!'}],
+            inventory: 7
         }
 
     ]
