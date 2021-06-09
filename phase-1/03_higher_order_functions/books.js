c34e7fcc95c14311f24457686761c5b7a4e8c4eb
ixnp <enprocopios@gmail.com>
2021-06-09 07:52:41 -0700

    starter code

---
+++ phase-1/03_higher_order_functions/books.js
@@ -0,0 +1,36 @@
+const bookStore = {
+    location: "Seattle",
+    inventory: [
+        {
+            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+            author: 'Marjin Haverbeke',
+            price: 10,
+            reviews: [{user_id: 1, content:'Good book, but not great for new coders'}]
+        },
+        {
+            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
+            author: 'Jon Duckett',
+            price: 45,
+            reviews: [{user_id: 15, content:'good way to learn JQuery'}]
+        },
+        {
+            title: 'JavaScript: The Good Parts',
+            author: 'Douglas Crockford',
+            price: 36,
+            reviews: [{user_id: 25, content:'I disagree with everything in this book'}, {user_id: 250, content:'Only JS book anyone needs'}]
+        },
+        {
+            title: 'JavaScript: The Definitive Guide',
+            author: 'David Flanagan',
+            price: 25,
+            reviews: [{user_id: 44, content:'Great intro to js book'}, {user_id: 350, content:'It really is the Definitive guide'}]
+        },
+        {
+            title: 'You Don’t Know JS',
+            author: 'David Flanagan',
+            price: 6,
+            reviews: [{user_id: 76, content:'You can find this for free online, no need to pay for it!'}]
+        }
+
+    ]
+}
---
+++ phase-1/03_higher_order_functions/index.js
@@ -0,0 +1,68 @@
+const orders = [
+    {
+        id: 13,
+        flavor: 'Vanilla',
+        size: '6" cake',
+        amount: 1,
+        price: 40.00,
+        ingredients: ["buttermilk", " vanilla extract", "eggs", "sugar", "butter", "baking soda", "baking powder", "flour"]
+    },
+    {
+        id: 14,
+        flavor: 'Raspberry Cardamon Rose',
+        size: '9" cake',
+        amount: 1,
+        price: 50.00,
+        ingredients: ["cardamon", "rose water", "buttermilk", " vanilla extract", "eggs", "sugar", "butter", "baking soda", "baking powder", "flour"]
+    },
+    {
+        id: 15,
+        flavor: 'Pink Champagne',
+        size: 'cup cake',
+        amount: 25,
+        price: 37.5,
+        ingredients: ["Andre Blush Champagne", "buttermilk", " vanilla extract", "eggs", "sugar", "butter", "baking soda", "baking powder", "flour"]
+    },
+    {
+        id: 1,
+        flavor: 'Earl Grey',
+        size: 'cup cake',
+        amount: 12,
+        price: 18.00,
+        ingredients: ["earl grey", "buttermilk", " vanilla extract", "eggs", "sugar", "butter", "baking soda", "baking powder", "flour"]
+    }
+]
+
+
+// forEach ****************************************************
+// Should loop through every item in an array and perform some action on every item in the array
+// return undefined
+
+
+
+// map ********************************************************
+// Should loop through every item in an array
+// it should take a transform function
+// transform every item in the array
+// should return a new array with all of the transformed items
+
+
+
+// find
+// Should loop through every item in an array
+// perform a test on every item in the array
+// should return the FIRST item that returns true in the test
+
+
+//filter
+//Should loop through every item in the array
+//perform a test on every item
+//Should return an array of items that returned true in the test
+
+
+// reduce
+// should loop through every item in an array
+// should have an accumulator that is passed to every iteration
+// perform some action involving the accumulator that will result in a
+// single value
+
